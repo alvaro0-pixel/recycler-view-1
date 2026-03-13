6599d0a0c146f63f18dd30cede0a3c7d4b8741ef
alvaro0-pixel <alvarodalmaso07@gmail.com>
2026-03-13 16:04:23 -0300

    Final changes

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerview.adapter.PostAdapter
+import com.example.recyclerview.data.model.Post
+import com.example.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var postAdapter: PostAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        setContentView(binding.root)
+        initRecyclerPost(getPost())
     }
+
+    private fun initRecyclerPost(postList: List<Post>) {
+        postAdapter = PostAdapter(postList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.adapter = postAdapter
+    }
+
+    private fun getPost() = listOf<Post>(
+        Post("0", "Post0"),
+        Post("1", "Post1"),
+        Post("2", "Post2"),
+        Post("3", "Post3")
+    )
+
+
 }
--- app/src/main/java/com/example/recyclerview/adapter/PostAdapter.kt
+++ app/src/main/java/com/example/recyclerview/adapter/PostAdapter.kt
@@ -1,11 +1,35 @@
 package com.example.recyclerview.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerview.data.model.Post
 import com.example.recyclerview.databinding.PostItemBinding
 
 class PostAdapter(
     var postList: List<Post>
-) {
+): RecyclerView.Adapter<PostAdapter.MyViewHolder>() {
+
+    // O método cria a visualização de cada item da lista quando ela precisa ser exibida na tela
+    // Esse método não preenche os dados no item. Ele apenas infla o layout do XML que representa cada item
+    override fun onCreateViewHolder(
+        parent: ViewGroup, // Item visual da lista
+        viewType: Int // Posição do item na lista
+    ): MyViewHolder {
+        val view = PostItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        // item visual criado
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: MyViewHolder, // item visual da lista
+        position: Int // posicao do item visual na lista
+    ) {
+        val post = postList[position]
+        holder.binding.txtTitle.text = post.description
+    }
+
+    override fun getItemCount() = postList.size
+
     inner class MyViewHolder(val binding: PostItemBinding): RecyclerView.ViewHolder(binding.root)
 }
